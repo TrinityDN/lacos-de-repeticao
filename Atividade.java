a1cf551ef0a545e5586c138d7f399ee3ba016542
Trinity <111662820+TrinityDN@users.noreply.github.com>
2023-02-21 10:42:05 -0300

    Add files via upload

---
+++ Atividade.java
@@ -0,0 +1,26 @@
+
+public class Atividade {
+	
+	public static void main(String [] args) {
+		
+		//somente numeros impares de 1 a 100
+		
+		for(int x = 1; x <= 100; x = x + 2) {
+			
+			System.out.println(x);
+			
+		}
+		
+		//somente numeros pares de 1 a 100
+		
+		
+		System.out.println("--------------");
+		
+		
+		for(int x = 0; x <= 100; x = x + 2) {
+			
+			System.out.println(x);
+		}
+	}
+
+}
---
+++ TesteRepeticao.java
@@ -0,0 +1,53 @@
+
+public class TesteRepeticao {
+	
+	//comentário de uma linha
+	//método principal mais 
+	
+	public static void main(String [] args) {
+		
+		//primeiro laco de repetição
+		//impressão dos números de 1 a 10
+		//for
+		
+		for(int x = 10; x > 0; x--) {
+			
+			//int x = 1 -> atribuição
+			// x < 11 -> condição, ou seja, ficara no for até 10
+			//x ++ -> incremento, ou seja, adiciona uma unidade de cada vez
+			// x++ -> x = x + 1
+			// x-- -> decremento
+			// x-- -> x  = x - 1
+			// x = x + 2 para incrementar a multiplicação, x vai de 2 em 2, duas unidades ao x
+			//imprimindo o valor de x
+			
+			System.out.println(x);
+			
+		}
+		
+			
+			for(int x = 1; x <= 100; x++) {
+				
+			
+				System.out.println(x);
+				
+			}
+			
+			System.out.println("--------------");
+			
+			for(int x = 100; x > 1; x--) {
+				
+				System.out.println(x);
+				
+			}
+				
+	}
+
+}
+
+//exibir os números em ordem decrescente
+
+
+	
+
+		
